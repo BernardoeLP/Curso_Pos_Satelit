2c48403c51cbcd0594d3c7f2c95d07407eef9f3e
Bernardo Eylenstein <bernardoe1602@gmail.com>
2024-09-12 08:10:58 -0300

    new file:   Practica_3/Ejercicio_2y3_V2.png 	modified:   Practica_3/TP_3-2y3_V2.py

--- Practica_3/TP_3-2y3_V2.py
+++ Practica_3/TP_3-2y3_V2.py
@@ -73,13 +73,15 @@ print("Armando nuevos dataframes con los bloques encontrados . . .")
 G1 = sat7[sat7["Grupo"] == 1]
 med = G1["L1L2"].mean()
 G1["L1L2-deb"] = G1["L1L2"] - med
-max1izq = G1["L1L2-deb"].max()
+# max1izq = G1["L1L2-deb"].max()
 
 valor_med = G1["P2-C1"].mean()
 G1["P2-C1_debiased"] = G1["P2-C1"]-valor_med
 max1der = G1["P2-C1_debiased"].max()
+max1izq = max1der
 min1der = G1["P2-C1_debiased"].min()
-min1izq = max1izq * min1der/max1der
+#min1izq = max1izq * min1der/max1der
+min1izq = min1der
 
 G1["PS3"] = G1["P2-C1_debiased"] - G1["L1L2-deb"]
 
@@ -90,13 +92,15 @@ G1["PS3"] = G1["P2-C1_debiased"] - G1["L1L2-deb"]
 G3 = sat7[sat7["Grupo"] == 3]
 med = G3["L1L2"].mean()
 G3["L1L2-deb"] = G3["L1L2"] - med
-max3izq = G3["L1L2-deb"].max()
+# max3izq = G3["L1L2-deb"].max()
 
 valor_med = G3["P2-C1"].mean()
 G3["P2-C1_debiased"] = G3["P2-C1"]-valor_med
 max3der = G3["P2-C1_debiased"].max()
+max3izq = max3der
 min3der = G3["P2-C1_debiased"].min()
-min3izq = max3izq * min3der/max3der
+#min3izq = max3izq * min3der/max3der
+min3izq = min3der
 
 G3["PS3"] = G3["P2-C1_debiased"] - G3["L1L2-deb"]
 
@@ -107,6 +111,10 @@ G4["L1L2-deb"] = G4["L1L2"] - med
 
 valor_med = G4["P2-C1"].mean()
 G4["P2-C1_debiased"] = G4["P2-C1"]-valor_med
+max4der = G4["P2-C1_debiased"].max()
+max4izq = max4der
+min4der = G4["P2-C1_debiased"].min()
+min4izq = min4der
 
 G4["PS3"] = G4["P2-C1_debiased"] - G4["L1L2-deb"]
 
@@ -117,6 +125,10 @@ G5["L1L2-deb"] = G5["L1L2"] - med
 
 valor_med = G5["P2-C1"].mean()
 G5["P2-C1_debiased"] = G5["P2-C1"]-valor_med
+max5der = G5["P2-C1_debiased"].max()
+max5izq = max5der
+min5der = G5["P2-C1_debiased"].min()
+min5izq = min5der
 
 G5["PS3"] = G5["P2-C1_debiased"] - G5["L1L2-deb"]
 
@@ -137,7 +149,7 @@ formatter = ticker.FormatStrFormatter('%1.2f')
 tformatter = mdates.DateFormatter('%H:%M')
 
 dotsize = 0.9
-margen = 1.05
+margen = 1.1
 
 ax[0].plot(G1["FechaHora"].astype('datetime64[us]'), G1["L1L2-deb"], 'tab:red')
 ax[0].set(ylim=(min1izq * margen, max1izq * margen))
@@ -154,15 +166,19 @@ ax1.set(ylim=(min3der * margen, max3der * margen))
 ax1.tick_params(axis="y", labelsize=7)
 
 ax[2].plot(G4["FechaHora"].astype('datetime64[us]'), G4["L1L2-deb"], 'tab:blue')
+ax[2].set(ylim=(min4izq * margen, max4izq * margen))
 ax[2].set(ylabel="L1L2 [m]")
 ax1 = ax[2].twinx()
 ax1.scatter(G4["FechaHora"].astype('datetime64[us]'), G4["P2-C1_debiased"], c='red', s=dotsize)
+ax1.set(ylim=(min4der * margen, max4der * margen))
 ax1.tick_params(axis="y", labelsize=7)
 ax1.set(ylabel='P2 - C1 [m]')
 
 ax[3].plot(G5["FechaHora"].astype('datetime64[us]'), G5["L1L2-deb"], 'tab:orange')
+ax[3].set(ylim=(min5izq * margen, max5izq * margen))
 ax1 = ax[3].twinx()
 ax1.scatter(G5["FechaHora"].astype('datetime64[us]'), G5["P2-C1_debiased"], c='blue', s=dotsize)
+ax1.set(ylim=(min5der * margen, max5der * margen))
 ax1.tick_params(axis="y", labelsize=7)
 
 for i in range(4):
